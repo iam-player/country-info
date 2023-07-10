2a3a0e4e12efd4a1be8e6c0d28e055612c5f2a20
iam-player <iampl4y3r@gmail.com>
2023-07-10 15:55:16 +0200

    feature: render static map image based on lon/lat from API

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,7 +17,9 @@ const countryCurrencies = document.getElementById("countryCurrencies");
 const countryRegion = document.getElementById("countryRegion");
 const countrySubRegion = document.getElementById("countrySubRegion");
 
-// async fetch from api
+
+
+// async fetch from country data api
 const getDataFromUrl = async () => {
 	// api url + user input
 	const api = `https://restcountries.com/v3.1/name/${search.value}`;
@@ -34,8 +36,8 @@ const getDataFromUrl = async () => {
 		});
 
 		if (response.status === 404) {
-			search.classList.add('error');
-			search.classList.add('bounce');
+			search.classList.add("error");
+			search.classList.add("bounce");
 		} else if (!response.ok) {
 			throw new Error(`HTTP protocol error: ${response.status}`);
 		} else {
@@ -53,29 +55,53 @@ const getDataFromUrl = async () => {
 				currency: Object.keys(countryJson.currencies).join(", "),
 				region: String(countryJson.region),
 				subRegion: String(countryJson.subregion),
+				latitude: countryJson.latlng[0],
+				longitude: countryJson.latlng[1],
 			};
 
 			// push data
-			dataHeader.innerText = countryModel.commonName + " (" + countryModel.capital + ")";
+			dataHeader.innerText =
+				countryModel.commonName + " (" + countryModel.capital + ")";
 			dataSubHeader.innerText = countryModel.officialName;
 			countryFlag.innerText = countryModel.flag;
 
 			// language(s) for more than 1 lang
 			if (Object.values(countryJson.languages).length > 1) {
-				countryLanguages.innerText = `Languages: ${countryModel.language}`
+				countryLanguages.innerText = `Languages: ${countryModel.language}`;
 			} else {
-				countryLanguages.innerText = `Language: ${countryModel.language}`
+				countryLanguages.innerText = `Language: ${countryModel.language}`;
 			}
 
 			// currency(ies) for more than 1 curr
 			if (Object.values(countryJson.currencies).length > 1) {
-				countryCurrencies.innerText = `Currencies: ${countryModel.currency}`
+				countryCurrencies.innerText = `Currencies: ${countryModel.currency}`;
 			} else {
-				countryCurrencies.innerText = `Currency: ${countryModel.currency}`
+				countryCurrencies.innerText = `Currency: ${countryModel.currency}`;
 			}
 
 			countryRegion.innerHTML = `Region: ${countryModel.region}`;
 			countrySubRegion.innerHTML = `Sub-region: ${countryModel.subRegion}`;
+
+			// async fetch from maps api
+			const getMapFromUrl = async () => {
+				const api = `https://tile.thunderforest.com/static/transport/${countryModel.longitude},${countryModel.latitude},5/500x300.png?apikey=f7f92df8257d4a87b3920f0f7542c27b`;
+				console.log("X");
+				try {
+					const response = await fetch(api, {
+						method: "GET",
+					});
+					console.log(response);
+
+					// google maps api with lng/lat query
+					const mapRender = document.getElementById("map");
+					mapRender.innerHTML = `<img src="${api}">`;
+
+				} catch (err) {
+					console.error(err);
+				}
+			};
+
+			getMapFromUrl();
 		}
 	} catch (err) {
 		console.error(err);
@@ -85,13 +111,12 @@ const getDataFromUrl = async () => {
 // get data on "Enter" keydown event
 search.addEventListener("keydown", (e) => {
 	//remove error class
-	search.classList.remove('error');
+	search.classList.remove("error");
 
 	// keyCode is deprecated - using .key & .code insteaed
 	if (e.code === "Enter" || e.key === "Enter" || e.isComposing === "true") {
 		getDataFromUrl();
-	} 
-	
+	}
 });
 
 // get data on click event
